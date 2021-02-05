1625d8e5103a91c9672c0c7082a1f916ff3f490f
kakashy <josephwawerug@gmail.com>
2021-02-05 06:22:39 +0300

    patch for output button

--- src/python/fileInfo.py
+++ src/python/fileInfo.py
@@ -1,4 +1,16 @@
 import bpy
+<<<<<<< Updated upstream
 
 scene = bpy.context.scene
 print("Scene %r frames: %d..%d = %d" % (scene.name, scene.frame_start, scene.frame_end - scene.frame_start + 1))
+=======
+import webbrowser
+import os
+
+ # Get absolute path:
+filepath = bpy.context.scene.render.filepath
+absolutepath = bpy.path.abspath(filepath)
+
+
+webbrowser.open(absolutepath)
+>>>>>>> Stashed changes
--- src/scriptz/srv.js
+++ src/scriptz/srv.js
@@ -1,10 +1,18 @@
+<<<<<<< Updated upstream
 let spawn = require("child_process").spawn;
+=======
+const spawn = require("child_process").spawn;
+>>>>>>> Stashed changes
 var path = require('path');
 var { shell, app } = require('electron').remote;
 // const fBar = document.getElementById("foodbar");
 const rOut = document.getElementById("render-out");
 const xBar = document.getElementById("snackbar");
 const lBtn = document.getElementById("logBtn");
+<<<<<<< Updated upstream
+=======
+const fBtn = document.getElementById("folderBtn");
+>>>>>>> Stashed changes
 
 var irenderer = document.getElementById("img-renderBtn");
 var arenderer = document.getElementById("anim-renderBtn");
@@ -84,6 +92,11 @@ irenderer.addEventListener("click", function(){
             // fBar.className = "show";
             // show log button
             lBtn.className = "show";
+<<<<<<< Updated upstream
+=======
+            // show folder button
+            fBtn.className = "show";
+>>>>>>> Stashed changes
 
             // After 3 seconds, remove the show class from DIV; EDIT: Commented out since a native notification might be a better choice
             // setTimeout(function(){ fBar.className = fBar.className.replace("show", ""); }, 3000);
@@ -126,6 +139,11 @@ arenderer.addEventListener("click", function(){
     };
         // Add the "show" class to DIV
         xBar.className = "show";
+<<<<<<< Updated upstream
+=======
+        // show folder button
+        fBtn.className = "show";
+>>>>>>> Stashed changes
 
         // After 3 seconds, remove the show class from DIV
         setTimeout(function(){ xBar.className = xBar.className.replace("show", ""); }, 3000);
@@ -148,6 +166,11 @@ bat.on("exit", (code) => {
 
         // fBar.className = "show";
         lBtn.className = "show";
+<<<<<<< Updated upstream
+=======
+        // show folder button
+        fBtn.className = "show";
+>>>>>>> Stashed changes
 
         const successNotification = new Notification ('CMD 4 Rendz', {
             body: 'Your render is complete'
@@ -178,3 +201,25 @@ lBtn.addEventListener("click", function(){
     shell.openPath(path.join(app.getPath("documents"), "CMD4Rendz", "Logs", "render-info.txt"));
 });
 
+<<<<<<< Updated upstream
+=======
+// open render folder
+fBtn.addEventListener("click", function(){
+    // test
+    fBtn.innerHTML = "Opening render folder"
+    let bwd = path.dirname(localStorage.getItem("filePath"));
+    let outputCommandString = 'pushd "' + bwd + '" && blender.exe -b "' + sessionStorage.getItem("blendPath") + '" -y -P "' + path.join(__dirname, '../python/fileInfo.py') + '"';
+    alert(outputCommandString);
+    let outputCommand = spawn(outputCommandString, {
+      shell: true,
+    });
+    outputCommand.stdout.on("data", (data) => {
+      alert(data);
+    });
+    outputCommand.stdout.on("err", (err) => {
+        alert(err);
+    });
+    fBtn.innerHTML = "Open render folder"
+});
+
+>>>>>>> Stashed changes
